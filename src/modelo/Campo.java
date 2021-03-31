fa15250339369e4d0335c27be3a61b42bbc8f26b
Charles Diego <demolock@hotmail.com>
2021-03-31 11:36:38 -0300

    Não consigo fazer o test

--- src/modelo/Campo.java
+++ src/modelo/Campo.java
@@ -1,5 +1,45 @@
 package modelo;
 
-public class Campo {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Campo {
+	
+	private boolean minado;
+	private boolean aberto;
+	private boolean marcado;
+	
+	private final int linha;
+	private final int coluna;
+	
+	private List<Campo> vizinhos = new ArrayList<>();
+	
+	
+	  public Campo (int linha, int coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+		
+	}
+	
+	boolean adicionarVizinho(Campo vizinho) {
+		boolean linhaDiferente = linha != vizinho.linha;
+		boolean colunaDiferente = coluna != vizinho.coluna;
+		boolean diagonal = linhaDiferente && colunaDiferente;
+		
+		int deltaLinha = Math.abs(linha-vizinho.linha);
+		int deltaColuna = Math.abs(coluna-vizinho.coluna);
+		int detalGeral = deltaColuna +  deltaLinha;
+		
+		if (detalGeral ==1 && !diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+			
+		} else if (detalGeral == 2 && diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		}else {
+		return false;
+		}
+	}
+	
 }
